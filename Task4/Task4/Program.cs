2eada23a7ad3bd4adad8103d0377857223c2cd7a
Sharique10-01 <110415184+Sharique10-01@users.noreply.github.com>
2025-08-28 15:37:55 +0530

    Add files via upload

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class SmartMeter
+{
+    public int MeterId { get; set; }
+    public string CustomerName { get; set; }
+
+    public SmartMeter(int meterId, string customerName)
+    {
+        MeterId = meterId;
+        CustomerName = customerName;
+    }
+}
+class ResidentialMeter : SmartMeter
+{
+    public string HouseType { get; set; }
+
+    public ResidentialMeter(int meterId, string customerName, string houseType)
+        : base(meterId, customerName)
+    {
+        HouseType = houseType;
+    }
+
+    public void PrintDetails()
+    {
+        Console.WriteLine($"Residential Meter -> ID: {MeterId}, Name: {CustomerName}, HouseType: {HouseType}");
+    }
+}
+class CommercialMeter : SmartMeter
+{
+    public string BusinessType { get; set; }
+
+    public CommercialMeter(int meterId, string customerName, string businessType)
+        : base(meterId, customerName)
+    {
+        BusinessType = businessType;
+    }
+
+    public void PrintDetails()
+    {
+        Console.WriteLine($"Commercial Meter -> ID: {MeterId}, Name: {CustomerName}, BusinessType: {BusinessType}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ResidentialMeter rMeter = new ResidentialMeter(201, "Alice", "Apartment");
+        CommercialMeter cMeter = new CommercialMeter(301, "Bob", "Shop");
+
+    
+        rMeter.PrintDetails();
+        cMeter.PrintDetails();
+    }
+}
---
+++ Task6/Task6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+abstract class MeterReading
+{
+    public int Units { get; set; }
+    public abstract int CalculateBill();
+}
+
+class ResidentialReading : MeterReading
+{
+    public ResidentialReading(int units) { Units = units; }
+    public override int CalculateBill() => Units * 5;
+}
+
+class CommercialReading : MeterReading
+{
+    public CommercialReading(int units) { Units = units; }
+    public override int CalculateBill() => Units * 8;
+}
+
+class Program
+{
+    static void Main()
+    {
+        MeterReading res = new ResidentialReading(100);
+        MeterReading com = new CommercialReading(100);
+
+        Console.WriteLine($"Residential Bill = {res.CalculateBill()}");
+        Console.WriteLine($"Commercial Bill = {com.CalculateBill()}");
+    }
+}
