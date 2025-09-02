dd06fd51655b5d52c7ead32437ecc1c89a2ab0a3
Sharique10-01 <110415184+Sharique10-01@users.noreply.github.com>
2025-09-02 11:54:42 +0530

    Add files via upload

---
+++ Task10/Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task DownloadFileAsync()
+    {
+        Console.WriteLine("Download Strated");
+        await Task.Delay(5000);
+        Console.WriteLine("Download complete!");
+    }
+
+    static async Task Main()
+    {
+        await DownloadFileAsync();
+    }
+}
---
+++ Task9/Task9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string path = "favorites.txt";
+        using (StreamWriter sw = new StreamWriter(path))
+        {
+            for (int i = 0; i < 5; i++)
+                sw.WriteLine(Console.ReadLine());
+        }
+        string[] colors = File.ReadAllLines(path);
+        foreach (string color in colors)
+            Console.WriteLine(color);
+    }
+}
